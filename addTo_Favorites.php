1f03e2616c7aa85fd671734999f2fb2ee02cf8bd
jhou45 <42850230+jhou45@users.noreply.github.com>
2019-12-01 22:08:19 -0800

    Update addTo_Favorites.php

--- addTo_Favorites.php
+++ addTo_Favorites.php
@@ -6,15 +6,17 @@
   //usage: check if food item is in favorites list
   function isItemExistsinFavorites($user, $item)
   {
-    $result = mysqli_query($conn, "SELECT * FROM Favorites WHERE Username = $user AND
-        FoodName = $item");
+    global $conn;
+    $result = mysqli_query($conn, "SELECT * FROM Favorites WHERE Username = '$user' AND
+        FoodName = '$item'");
     return mysqli_num_rows($result);
   }
 
   //inputs: username, food item name
   //usage: to add items from inventory list to favorites list
-  function onFavoritesAddInventoryItem($user, $item)
+  function onFavoritesAddInventoryItem($conn, $user, $item)
   {
+    global $conn;
     if(isItemExistsinFavorites($user, $item) > 0)
     {
       echo "This item is already in your favorites";
@@ -22,8 +24,8 @@
     else
     {
       $sql = "INSERT INTO Favorites (Username, FoodName, Quantity, ShelfLifeInDays) SELECT
-          Username, FoodName, Quantity, ShelfLifeInDays FROM Inventory WHERE Username = $user
-          AND FoodName = $item";
+          Username, FoodName, Quantity, ShelfLifeInDays FROM Inventory WHERE Username = '$user'
+          AND FoodName = '$item'";
       mysqli_query($conn, $sql);
     }
   }
@@ -32,6 +34,7 @@
   //usage: to add items from grocery list to favorites list
   function onFavoritesAddGroceryListItem($user, $item)
   {
+    global $conn;
     if(isItemExistsinFavorites($user, $item) > 0)
     {
       echo "This item is already in your favorites";
@@ -39,8 +42,8 @@
     else
     {
       $sql = "INSERT INTO Favorites (Username, FoodName, Quantity, ShelfLifeInDays) SELECT
-          Username, FoodName, Quantity, ShelfLifeInDays FROM GroceryList WHERE Username = $user
-          AND FoodName = $item";
+          Username, FoodName, Quantity, ShelfLifeInDays FROM GroceryList WHERE Username = '$user'
+          AND FoodName = '$item'";
       mysqli_query($conn, $sql);
     }
   }
@@ -49,19 +52,20 @@
   //usage: to add item from food items list to favorites list
   function onFavoritesAddFoodItem($user, $item, $quantity)
   {
+    global $conn;
     if(isItemExistsinFavorites($user, $item) > 0)
     {
       echo "This item is already in your favorites";
     }
     else
     {
-      mysqli_query($conn, "INSERT INTO Favorites (Username, FoodName) VALUES $user, $item");
-      mysqli_query($conn, "UPDATE Favorites SET Quanity = $quanity WHERE Username = $user
-          AND FoodName = $item");
-      $result = mysqli_query($conn, "SELECT ShelfLifeInDays FROM FoodItems WHERE FoodName = $item");
+      mysqli_query($conn, "INSERT INTO Favorites (Username, FoodName) VALUES '$user', '$item'");
+      mysqli_query($conn, "UPDATE Favorites SET Quanity = '$quanity' WHERE Username = '$user'
+          AND FoodName = '$item'");
+      $result = mysqli_query($conn, "SELECT ShelfLifeInDays FROM FoodItems WHERE FoodName = '$item'");
       $daysToExpr = mysqli_fetch_field($result);
-      mysqli_query($conn, "UPDATE Favorites SET ShelfLifeInDays = $daysToExpr WHERE Username = $user
-          AND FoodName = $item");
+      mysqli_query($conn, "UPDATE Favorites SET ShelfLifeInDays = '$daysToExpr' WHERE Username = '$user'
+          AND FoodName = '$item'");
     }
   }
 ?>
