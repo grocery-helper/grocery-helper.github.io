0885913055cf8be3a597e4b973eb59e0087d05a9
jhou45 <42850230+jhou45@users.noreply.github.com>
2019-12-01 22:17:52 -0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -6,8 +6,8 @@
     $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
     // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    if (!$conn) {
+        die("Connection failed: ");
     }
     echo "Connected successfully";
 
