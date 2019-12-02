2947d77c47970a043b42b4c10443ffc957ba34bc
jhou45 <42850230+jhou45@users.noreply.github.com>
2019-12-01 22:14:10 -0800

    Update addTo_inventory.php

--- addTo_inventory.php
+++ addTo_inventory.php
@@ -11,20 +11,23 @@
   //if item on grocery list is marked then copy entry into inventory
   function setPurchaseDate($user, $item)
   {
+    global $conn;
     $today = date("Y/m/d");
-    mysqli_query($conn, "UPDATE Inventory SET PurchaseDate = $today WHERE Username = $user AND FoodName = $item");
+    mysqli_query($conn, "UPDATE Inventory SET PurchaseDate = '$today' WHERE Username = '$user' AND FoodName = '$item'");
     return $today;
   }
 
   function getShelfLife($user, $item)
   {
-    $result = mysqli_query($conn, "SELECT ShelfLifeInDays FROM FoodItems WHERE FoodName = $item");
+    global $conn;
+    $result = mysqli_query($conn, "SELECT ShelfLifeInDays FROM FoodItems WHERE FoodName = '$item'");
     $daysToExpr = mysqli_fetch_field($result);
     return $daysToExpr;
   }
 
   function setExpDate($user, $item, $today)
   {
+    global $conn;
     $shelfLife = getShelfLife($user, $item);
 
     if($shelfLife > 1)
@@ -36,19 +39,20 @@
       $expiration = strtotime("+$daysToExpr day", strtotime($today));
     }
 
-    $sql = "UPDATE Inventory SET ExpDate = $expiration WHERE FoodName = $item AND Username = $user";
-    if ($conn->query($sql) === TRUE) {
+    $sql = "UPDATE Inventory SET ExpDate = '$expiration' WHERE FoodName = '$item' AND Username = '$user'";
+    if (mysqli_query($conn, $sql)) {
         echo "Item added successfully";
     } else {
-        echo "Error adding item: " . $conn->error;
+        echo "Error adding item: ";
     }
   }
 
   function onInventoryAddGroceryItem($user, $item)
   {
+    global $conn;
     $sql = "INSERT INTO Inventory (Username, FoodName, Quantity, ShelfLifeInDays) SELECT
-        Username, FoodName, Quantity, ShelfLifeInDays FROM GroceryList WHERE Username = $user
-        AND FoodName = $item";
+        Username, FoodName, Quantity, ShelfLifeInDays FROM GroceryList WHERE Username = '$user'
+        AND FoodName = '$item'";
 
     mysqli_query($conn, $sql);
 
