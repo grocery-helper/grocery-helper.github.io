d530811e33f60ea78ca1d1928d7fb09a525e95ea
jhou45 <42850230+jhou45@users.noreply.github.com>
2019-12-01 22:11:34 -0800

    Update addTo_groceryList.php

--- addTo_groceryList.php
+++ addTo_groceryList.php
@@ -13,60 +13,64 @@
   //        False - food item does not exist in Grocery List
   function isItemExists($user, $item)
   {
-    $result = mysqli_query($conn, "SELECT * FROM GroceryList WHERE Username = $user AND
-        FoodName = $item");
+    global $conn;
+    $result = mysqli_query($conn, "SELECT * FROM GroceryList WHERE Username = '$user' AND
+        FoodName = '$item'");
     return mysqli_num_rows($result);
   }
 
 
   function onGroceryListInsertFoodItem($user, $item, $quantity)
   {
-    $sql = "INSERT INTO GroceryList (Username, FoodName) VALUES $user, $item ";
+    global $conn;
+    $sql = "INSERT INTO GroceryList (Username, FoodName) VALUES '$user', '$item'";
     mysqli_query($conn, $sql);
 
-    $sql = "SELECT ShelfLifeInDays FROM FoodItems WHERE FoodName = $item";
+    $sql = "SELECT ShelfLifeInDays FROM FoodItems WHERE FoodName = '$item'";
 
     if ($result = mysqli_query($conn, $sql)) {
         $shelfLife = mysqli_fetch_field($result);
-        mysqli_query($conn, "UPDATE GroceryList SET Quantity = $quantity, ShelfLifeInDays = $shelfLife
-            WHERE Username = $user AND FoodName = $item");
+        mysqli_query($conn, "UPDATE GroceryList SET Quantity = '$quantity', ShelfLifeInDays = '$shelfLife'
+            WHERE Username = '$user' AND FoodName = '$item'");
         echo "Item added successfully";
     } else {
-        echo "Error adding item: " . $conn->error;
+        echo "Error adding item ";
     }
   }
 
 
   function onGroceryListUpdateFoodItem($user, $item, $quantity)
   {
-    $sql = "SELECT Quantity FROM GroceryList WHERE Username = $user AND
-        FoodName = $item";
+    global $conn;
+    $sql = "SELECT Quantity FROM GroceryList WHERE Username = '$user' AND
+        FoodName = '$item'";
     $result = mysqli_query($conn, $sql);
     $curQuanity = mysqli_fetch_field($result);
     $newQuantity = $quanity + $curquantity;
-    $sql = "UPDATE GroceryList SET Quantity = $newQuantity WHERE Username = $user AND
-        FoodName = $item";
+    $sql = "UPDATE GroceryList SET Quantity = '$newQuantity' WHERE Username = '$user' AND
+        FoodName = '$item'";
 
     if ($conn->query($sql) === TRUE) {
         echo "Item updated successfully";
     } else {
-        echo "Error updating item: " . $conn->error;
+        echo "Error updating item ";
     }
   }
 
   //ADDING ITEM TO GROCERY LIST FROM FAVORITES LIST
   function onGroceryListInsertFavoritesItem($user, $item, $quantity)
   {
+    global $conn;
     if(isItemExists($user, $item) == 0)
     {
       $sql = "INSERT INTO GroceryList (Username, FoodName, Quantity, ShelfLifeInDays)
           SELECT Username, FoodName, Quantity, ShelfLifeInDays FROM Favorites WHERE
-          Username = $user and FoodName = $item";
+          Username = '$user' and FoodName = '$item'";
 
       if ($conn->query($sql) === TRUE) {
           echo "Item added successfully";
       } else {
-          echo "Error adding item: " . $conn->error;
+          echo "Error adding item";
       }
     }
   }
